01fe1c27b4fc41bc46f54c5c0b86283a9af57858
boyswan <jackwilliamboyce@gmail.com>
2017-03-09 23:30:02 +0000

    comment

--- index.js
+++ index.js
@@ -22,18 +22,17 @@ const eitherLogOrShow = Either.either(
   x => console.log("Total:", x)
 );
 
-const addCaliTax = tax(0.1); //10%
+const addCaliTax = tax(0.1); // 10% tax
 const apply25PercDisc = discount(0.25); // 25% discount
 
 const getItemPrice = item => Right(item.price);
 
-//api
 const showTotalPrice = item =>
   eitherLogOrShow(getItemPrice(item).chain(apply25PercDisc).chain(addCaliTax));
 
 const tShirt = { name: "t-shirt", price: 11 };
-const pant = { name: "t-shirt", price: "10 dollars" }; //error
-const chips = { name: "t-shirt", price: 5 }; //less than 10 dollars error
+const pant = { name: "t-shirt", price: "10 dollars" };
+const chips = { name: "t-shirt", price: 5 };
 
 showTotalPrice(tShirt); // Total Is: 9.075
 showTotalPrice(pant); // Error: Price must be numeric
